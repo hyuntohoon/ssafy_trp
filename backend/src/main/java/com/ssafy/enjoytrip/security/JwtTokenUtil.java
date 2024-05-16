5a5a5022bce1e6d0a6766295f997978ed5fd45d5
parkjihoon <zlxlgus123@naver.com>
2024-05-16 11:42:23 +0900

    feat : JwtTokenUtil generateToken 추가

---
+++ backend/src/main/java/com/ssafy/enjoytrip/security/JwtTokenUtil.java
@@ -0,0 +1,27 @@
+package com.ssafy.enjoytrip.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenUtil {
+
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512); // 비밀 키 생성
+    private final long EXPIRATION_TIME = 86400000; // 1일
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+}

d4875661049043bd1c1ce768e099f4a0d6ea549d
sangbeom <picel@picel.net>
2024-05-16 15:02:56 +0900

    feat: Remove unused API endpoints and add search functionality

--- frontend/src/api/search.js
+++ frontend/src/api/search.js
@@ -2,14 +2,6 @@ import { localAxios } from "@/utils/http-commons";
 
 const axios = localAxios();
 
-// export const getSido = (success, fail) => {
-//   axios.get("/areasido/sido").then(success).catch(fail);
-// };
-
-// export const getGugun = (sido, success, fail) => {
-//   axios.get(`/areasido/gugun/${sido}`).then(success).catch(fail);
-// };
-
 export const getSido = () => {
   return axios.get("/areasido/sido");
 };

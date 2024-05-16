85024e80126c0971f671ec8ebe323947f0203d0e
parkjihoon <zlxlgus123@naver.com>
2024-05-16 11:55:59 +0900

    feat : AuthService 생성 및 signIn 함수 추가

---
+++ backend/src/main/java/com/ssafy/enjoytrip/model/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.ssafy.enjoytrip.model.service;
+
+import com.ssafy.enjoytrip.model.dto.User;
+import com.ssafy.enjoytrip.model.service.UserService;
+import com.ssafy.enjoytrip.security.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    public String signIn(User user) {
+        User existingUser = userService.selectUserById(user);
+        if (existingUser != null) {
+            return jwtTokenUtil.generateToken(existingUser.getName());
+        }
+        return null;
+    }
+}

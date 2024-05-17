0b2875077d64835a27d5debf4779aafca198f7cc
parkjihoon <zlxlgus123@naver.com>
2024-05-17 14:36:10 +0900

    feat : setTripPlan 추가

--- backend/src/main/java/com/ssafy/enjoytrip/model/service/TripPlanServiceImpl.java
+++ backend/src/main/java/com/ssafy/enjoytrip/model/service/TripPlanServiceImpl.java
@@ -3,16 +3,17 @@ package com.ssafy.enjoytrip.model.service;
 import java.util.List;
 
 import com.ssafy.enjoytrip.model.dto.AttractionInfo;
+import com.ssafy.enjoytrip.model.dto.TripPlanWithPlaces;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.enjoytrip.model.dao.TripPlanDao;
 import com.ssafy.enjoytrip.model.dto.TripPlan;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TripPlanServiceImpl implements TripPlanService {
 
-	private TripPlanDao tripPlanDao;
+	private final TripPlanDao tripPlanDao;
 
 	public TripPlanServiceImpl(TripPlanDao tripPlanDao) {
 		this.tripPlanDao = tripPlanDao;
@@ -32,32 +33,30 @@ public class TripPlanServiceImpl implements TripPlanService {
 
 	@Override
 	@Transactional
-	public boolean setTripPlan(TripPlan tripPlan) {
+	public TripPlan setTripPlan(String name, String userId, List<Integer> attractionIds) {
+		TripPlan tripPlan = new TripPlan();
+		tripPlan.setName(name);
+		tripPlan.setUserId(userId);
+
 		// TripPlan 저장
-		int rowsAffected = tripPlanDao.setTripPlan(tripPlan);
-		if (rowsAffected > 0) {
-			// TripPlan과 관련된 장소 저장
-			for (AttractionInfo attractionInfo : tripPlan.getPlaces()) {
-				tripPlanDao.addPlaceToTripPlan(tripPlan.getId(), attractionInfo.getContentId());
-			}
-			return true;
+		tripPlanDao.setTripPlan(tripPlan);
+		int tripPlanId = tripPlan.getId(); // 새로 생성된 TripPlan의 ID
+
+		for (Integer attractionId : attractionIds) {
+			tripPlanDao.addPlaceToTripPlan(tripPlanId, attractionId);
 		}
+		return tripPlan;
+	}
+
+	@Override
+	@Transactional
+	public boolean updateTripPlan(TripPlan tripPlan) {
 		return false;
 	}
 
 	@Override
-	public List<TripPlan> getTripPlan(String userId) {
-		return tripPlanDao.getTripPlan(userId);
-	}
-
-	@Override
-	public boolean updateTripPlan(TripPlan tripPlan) {
-		return tripPlanDao.updateTripPlan(tripPlan);
-	}
-
-	@Override
+	@Transactional
 	public boolean delTripPlan(int id) {
-		return delTripPlan(id);
+		return false;
 	}
-
 }

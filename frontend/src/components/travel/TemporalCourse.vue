e9408e23c2d5d401f87d3c282bcb10837723c24e
sangbeom <picel@picel.net>
2024-05-17 15:08:47 +0900

    feat: Add TemporalCourse component to TravelView

---
+++ frontend/src/components/travel/TemporalCourse.vue
@@ -0,0 +1,113 @@
+<script setup>
+import PlaceCard from "@/components/common/PlaceCard.vue";
+
+import { useSearchStore } from "@/stores/search";
+const searchStore = useSearchStore();
+
+import { useRouteStore } from "@/stores/route";
+const routeStore = useRouteStore();
+
+import { storeToRefs } from "pinia";
+const { focus } = storeToRefs(searchStore);
+const { placeList } = storeToRefs(routeStore);
+console.log(placeList.value);
+
+const moveFocus = (data) => {
+  focus.value = { lat: data.latitude, lng: data.longitude, level: 3 };
+};
+
+const removePlace = (data) => {
+  const index = placeList.value.findIndex((place) => place.contentId === data.contentId);
+  if (index === -1) {
+    return;
+  }
+  placeList.value.splice(index, 1);
+};
+</script>
+
+<template>
+  <div class="wrap">
+    <h2>임시 코스</h2>
+    <h5><i class="bi bi-plus-circle" /> 아이콘을 눌러 코스에 장소를 추가하세요</h5>
+    <hr />
+    <span v-if="placeList.length === 0">코스에 장소가 없습니다.</span>
+    <div class="row">
+      <div class="col-6 card-wrap" v-for="place in placeList" :key="place.contentId">
+        <PlaceCard :data="place">
+          <template v-slot:actions>
+            <div class="button-wrap">
+              <button @click="moveFocus(place)">
+                <i class="bi bi-geo-alt"></i>
+              </button>
+              <button>
+                <i class="bi bi-heart"></i>
+              </button>
+              <button @click="removePlace(place)">
+                <i class="bi bi-x-circle"></i>
+              </button>
+            </div>
+          </template>
+        </PlaceCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  margin-top: 1rem;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(10px);
+  padding: 1rem;
+  border-radius: 10px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+button {
+  background-color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 1.5rem;
+}
+
+button:hover {
+  color: var(--accent-color);
+  transition: color 0.3s;
+}
+
+button:active {
+  color: var(--accent-color-dark-8);
+}
+
+.button-wrap {
+  display: flex;
+  justify-content: end;
+  margin-top: 1rem;
+}
+
+@keyframes scale-in-center {
+  0% {
+    transform: scale(0.5);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+@keyframes scale-out-center {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(0.5);
+    opacity: 0;
+  }
+}
+
+.card-wrap {
+  animation: scale-in-center 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+</style>

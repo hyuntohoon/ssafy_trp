f9a9af84c852a4c5222eb3d8a08de5f515d79eaa
sangbeom <picel@picel.net>
2024-05-17 12:26:05 +0900

    chore: Update SearchResult.vue to use pinia storeToRefs for resultData and add conditional rendering for empty search results

--- frontend/src/components/travel/SearchResult.vue
+++ frontend/src/components/travel/SearchResult.vue
@@ -1,11 +1,30 @@
-<script setup></script>
+<script setup>
+import SearchResultItem from "@/components/travel/SearchResultItem.vue";
+import { useSearchStore } from "@/stores/search";
+
+const searchStore = useSearchStore();
+
+import { storeToRefs } from "pinia";
+
+const { resultData } = storeToRefs(searchStore);
+</script>
 
 <template>
   <div class="search-result">
-    <h3>검색 결과</h3>
-    <div>
-      <p>검색 해주세용</p>
-    </div>
+    <h2 v-if="resultData.length === 0">검색 결과가 없습니다.</h2>
+    <SearchResultItem v-for="data in resultData" :key="data.id" :result="data" />
+    <!-- <ul>
+      <li v-for="result in resultData" :key="result.id">
+        <p>{{ result.addr1 }}</p>
+        <p>{{ result.addr2 }}</p>
+        <p>{{ result.zipcode }}</p>
+        <p>{{ result.title }}</p>
+        <p>{{ result.type }}</p>
+        <p>{{ result.tel }}</p>
+        <p>{{ result.latitude }}</p>
+        <p>{{ result.longitude }}</p>
+      </li>
+    </ul> -->
   </div>
 </template>
 
@@ -16,5 +35,8 @@
   backdrop-filter: blur(10px);
   padding: 1rem;
   border-radius: 10px;
+  height: 54.7vh;
+  overflow-y: auto;
+  overflow-x: hidden;
 }
 </style>

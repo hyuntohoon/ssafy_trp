5ffb018f95d0bbafb3b1888738ce0237a0490277
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-05-18 17:11:25 +0900

    feat : Post content_id, photo 추가 및 mapper 추가

--- backend/src/main/java/com/ssafy/enjoytrip/model/dto/Post.java
+++ backend/src/main/java/com/ssafy/enjoytrip/model/dto/Post.java
@@ -9,56 +9,98 @@ public class Post {
     private String content;
     private Timestamp createTimeStamp;
     private Timestamp updateTimeStamp;
+    private int content_id;
+    private String photo;
+
     @Override
     public String toString() {
-        return "Post [postId=" + postId + ", userId=" + userId + ", title=" + title + ", content=" + content
-                + ", createTimeStamp=" + createTimeStamp + ", updateTimeStamp=" + updateTimeStamp + "]";
+        return "Post{" +
+                "postId=" + postId +
+                ", userId='" + userId + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createTimeStamp=" + createTimeStamp +
+                ", updateTimeStamp=" + updateTimeStamp +
+                ", content_id=" + content_id +
+                ", photo='" + photo + '\'' +
+                '}';
     }
-    public Post() {
-    }
-    public Post(Integer postId, String userId, String title, String content, Timestamp createTimeStamp,
-                Timestamp updateTimeStamp) {
-        this.postId = postId;
-        this.userId = userId;
-        this.title = title;
-        this.content = content;
-        this.createTimeStamp = createTimeStamp;
-        this.updateTimeStamp = updateTimeStamp;
-    }
+
     public Integer getPostId() {
         return postId;
     }
+
     public void setPostId(Integer postId) {
         this.postId = postId;
     }
+
     public String getUserId() {
         return userId;
     }
+
     public void setUserId(String userId) {
         this.userId = userId;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getContent() {
         return content;
     }
+
     public void setContent(String content) {
         this.content = content;
     }
+
     public Timestamp getCreateTimeStamp() {
         return createTimeStamp;
     }
+
     public void setCreateTimeStamp(Timestamp createTimeStamp) {
         this.createTimeStamp = createTimeStamp;
     }
+
     public Timestamp getUpdateTimeStamp() {
         return updateTimeStamp;
     }
+
     public void setUpdateTimeStamp(Timestamp updateTimeStamp) {
         this.updateTimeStamp = updateTimeStamp;
+    }
+
+    public int getContent_id() {
+        return content_id;
+    }
+
+    public void setContent_id(int content_id) {
+        this.content_id = content_id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Post() {
+    }
+
+    public Post(Integer postId, String userId, String title, String content, Timestamp createTimeStamp, Timestamp updateTimeStamp, int content_id, String photo) {
+        this.postId = postId;
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.createTimeStamp = createTimeStamp;
+        this.updateTimeStamp = updateTimeStamp;
+        this.content_id = content_id;
+        this.photo = photo;
     }
 }

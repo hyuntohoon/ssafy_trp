ab5a2f0870d1fbdf9834fecfa2b21b5edd11db7e
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-05-20 03:35:23 +0900

    feat : comment jpa로 변경

--- backend/src/main/java/com/ssafy/enjoytrip/model/service/CommentServiceImpl.java
+++ backend/src/main/java/com/ssafy/enjoytrip/model/service/CommentServiceImpl.java
@@ -1,55 +1,62 @@
 package com.ssafy.enjoytrip.model.service;
 
-import java.util.List;
-
-import com.ssafy.enjoytrip.model.dao.CommentDao;
-import com.ssafy.enjoytrip.model.dto.Comment;
+import com.ssafy.enjoytrip.model.entity.Comment;
+import com.ssafy.enjoytrip.model.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.enjoytrip.model.dto.User;
+import java.util.List;
+import java.util.Optional;
 
-/**
- * CommentServiceImpl
- */
 @Service
 public class CommentServiceImpl implements CommentService {
 
-    private final CommentDao commentDao;
-
-    public CommentServiceImpl(CommentDao commentDao) {
-        this.commentDao = commentDao;
-    }
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Override
     public Comment addComment(Comment comment) {
-        return commentDao.addComment(comment);
+        return commentRepository.save(comment);
     }
 
     @Override
     public List<Comment> getAllComments() {
-        return commentDao.getAllComments();
+        return commentRepository.findAll();
     }
 
     @Override
     public Comment getCommentById(int commentId) {
-        return commentDao.getCommentById(commentId);
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        return comment.orElse(null);
     }
 
     @Override
-    public Comment updateComment(int commentId, Comment comment) {
-        return commentDao.updateComment(commentId, comment);
+    public Comment updateComment(int commentId, Comment commentDetails) {
+        Comment comment = getCommentById(commentId);
+        if (comment != null) {
+            comment.setPost(commentDetails.getPost());
+            comment.setUser(commentDetails.getUser());
+            comment.setContent(commentDetails.getContent());
+            comment.setUpdateTimeStamp(commentDetails.getUpdateTimeStamp());
+            comment.setLikes(commentDetails.getLikes());
+            return commentRepository.save(comment);
+        }
+        return null;
     }
 
     @Override
     public boolean deleteComment(int commentId) {
-        return commentDao.deleteComment(commentId);
+        if (commentRepository.existsById(commentId)) {
+            commentRepository.deleteById(commentId);
+            return true;
+        }
+        return false;
     }
 
     @Profile("test")
     @Override
     public void deleteAllComments() {
-        commentDao.deleteAllComments();
+        commentRepository.deleteAll();
     }
 }

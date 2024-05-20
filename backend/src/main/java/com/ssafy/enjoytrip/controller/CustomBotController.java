266572cbfe8cfbbf1bd3423278a44d04e99eabeb
parkjihoon <zlxlgus123@naver.com>
2024-05-20 15:48:53 +0900

    feat : chatGPT 기능 추가

---
+++ backend/src/main/java/com/ssafy/enjoytrip/controller/CustomBotController.java
@@ -0,0 +1,32 @@
+package com.ssafy.enjoytrip.controller;
+
+import com.ssafy.enjoytrip.model.dto.ChatGPTRequest;
+import com.ssafy.enjoytrip.model.dto.ChatGPTResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/bot")
+public class CustomBotController {
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(name = "prompt")String prompt){
+        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGPTResponse =  template.postForObject(apiURL, request, ChatGPTResponse.class);
+        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+    }
+
+}

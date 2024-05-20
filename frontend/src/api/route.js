8000c2741d43cf62829602324dde59e0226fb83a
sangbeom <picel@picel.net>
2024-05-20 14:54:15 +0900

    feat: Update API route for getting trip plans by user ID

--- frontend/src/api/route.js
+++ frontend/src/api/route.js
@@ -7,5 +7,5 @@ export const postTripPlan = (name, userId, attractionIds) => {
 };
 
 export const getTripPlans = (userId) => {
-  return axios.get(`/tripplan/user/${userId}`);
+  return axios.get(`/tripplan/tripPlans/places?userId=${userId}`);
 };

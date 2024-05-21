c23364331f10a8c6e117e4b0f28dd9340c7d15e6
parkjihoon <zlxlgus123@naver.com>
2024-05-21 11:49:01 +0900

    fix : findTop8ByKeywordAndContentTypeIdOrderByReadcountDesc limit 8개 수정

--- backend/src/main/java/com/ssafy/enjoytrip/model/repository/AttractionInfoRepository.java
+++ backend/src/main/java/com/ssafy/enjoytrip/model/repository/AttractionInfoRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface AttractionInfoRepository extends JpaRepository<AttractionInfo, Integer>, JpaSpecificationExecutor<AttractionInfo> {
     // 추가적인 쿼리 메서드가 필요하다면 여기에 정의
-    @Query("SELECT a FROM AttractionInfo a WHERE a.addr1 LIKE %:keyword% AND a.contentTypeId = :contentTypeId ORDER BY a.readcount DESC")
+    @Query("SELECT a FROM AttractionInfo a WHERE a.addr1 LIKE %:keyword% AND a.contentTypeId = :contentTypeId ORDER BY a.readcount DESC limit 8")
     List<AttractionInfo> findTop8ByKeywordAndContentTypeIdOrderByReadcountDesc(@Param("keyword") String keyword, @Param("contentTypeId") int contentTypeId);
 }

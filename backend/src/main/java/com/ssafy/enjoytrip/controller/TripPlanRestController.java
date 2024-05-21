4406a9b5eee2c430f8051bfc7fd9711752d73225
parkjihoon <zlxlgus123@naver.com>
2024-05-21 09:44:24 +0900

    style : 주석 삭제

--- backend/src/main/java/com/ssafy/enjoytrip/controller/TripPlanRestController.java
+++ backend/src/main/java/com/ssafy/enjoytrip/controller/TripPlanRestController.java
@@ -35,15 +35,6 @@ public class TripPlanRestController {
         }
     }
 
-//    @GetMapping("/user/{userId}")
-//    public ResponseEntity<List<TripPlanWithPlaces>> getTripPlansWithPlacesByUserId(@PathVariable String userId) {
-//        List<TripPlanWithPlaces> tripPlans = tripPlanService.getTripPlansWithPlacesByUserId(userId);
-//        if (tripPlans == null || tripPlans.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-//        }
-//        return ResponseEntity.ok(tripPlans);
-//    }
-
     @PostMapping
     public ResponseEntity<TripPlan> createTripPlan(@RequestBody TripPlanRequest tripPlanRequest) {
         TripPlan tripPlan = tripPlanService.createTripPlan(tripPlanRequest.getName(),tripPlanRequest.getUserId(),tripPlanRequest.getAttractionIds());

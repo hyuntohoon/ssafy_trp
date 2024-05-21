05ae5d3700500d952b7015c6a4553b7bc080008f
parkjihoon <zlxlgus123@naver.com>
2024-05-21 10:02:39 +0900

    feat : CustomBotController에 프롬포트 작성

--- backend/src/main/java/com/ssafy/enjoytrip/controller/CustomBotController.java
+++ backend/src/main/java/com/ssafy/enjoytrip/controller/CustomBotController.java
@@ -24,6 +24,8 @@ public class CustomBotController {
 
     @GetMapping("/chat")
     public String chat(@RequestParam(name = "prompt")String prompt){
+        String defaultPromt = "당신은 여행계획를 돕은 여행 계획 도우미입니다. 아래의 여행 정보와 날씨 정보를 제공할테니, 여행 계획 설립에 도움을 주세요.";
+        prompt+=defaultPromt;
         ChatGPTRequest request = new ChatGPTRequest(model, prompt);
         ChatGPTResponse chatGPTResponse =  template.postForObject(apiURL, request, ChatGPTResponse.class);
         return chatGPTResponse.getChoices().get(0).getMessage().getContent();
